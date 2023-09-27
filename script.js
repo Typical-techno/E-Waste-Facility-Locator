d238a343eb316211e00b048302a5ad4eecfb03c3
Typical-Techno <virusofhacking@gmail.com>
2023-09-27 16:57:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -28,10 +28,10 @@ let currentMessageIndex = 0;
 
 function hidePopup() {
     popup.style.display = 'none';
-    setTimeout(shoswPopup, 30000);
+    setTimeout(showPopup, 30000);
 }
 
-setTimeout(shoswPopup, 3000);
+setTimeout(showPopup, 3000);
 
 ;
 
