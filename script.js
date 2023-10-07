828672beb5ee297bacff25bf88571bde17b47b21
Typical-Techno <virusofhacking@gmail.com>
2023-10-07 20:58:44 +0530

    Update

--- script.js
+++ script.js
@@ -39,21 +39,21 @@ setTimeout(showPopup, 3000);
 
 // About Section Counter
 document.addEventListener("DOMContentLoaded", function () {
-    let valueDisplays = document.querySelectorAll(".num");
-    let interval = 1000;
-    valueDisplays.forEach((valueDisplay) => {
-        let startValue = 0;
-        let endValue = parseInt(valueDisplay.getAttribute("data-val"));
-        let duration = Math.floor(interval / endValue);
-        let counter = setInterval(function () {
-            startValue += 1;
-            valueDisplay.textContent = startValue;
-            if (startValue == endValue) {
-            clearInterval(counter);
-        }
-        }, duration);
-    });
-    });
+  let valueDisplays = document.querySelectorAll(".num");
+  let interval = 1000;
+  valueDisplays.forEach((valueDisplay) => {
+      let startValue = 0;
+      let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+      let duration = Math.floor(interval / endValue);
+      let counter = setInterval(function () {
+          startValue += 1;
+          valueDisplay.textContent = startValue;
+          if (startValue == endValue) {
+          clearInterval(counter);
+      }
+      }, duration);
+  });
+  });
 // About Section Counter
 
 const register_form = document.getElementById('Register');
@@ -90,3 +90,4 @@ function Logout() {
   localStorage.removeItem('token')
   window.location.reload()
 }
+
